ab9fea92f5e7b35b70e87999d5020e83c855adda
Rafael Matias <rafael@skyle.net>
2022-12-14 18:18:04 +0100

    force install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ansible-galaxy install -r requirements.yml
-ansible-galaxy collection install -r requirements.yml
+ansible-galaxy install -r requirements.yml --force
+ansible-galaxy collection install -r requirements.yml --force
