9216cae989da0fd3cab9ff82bd00bdd37fe84335
parithosh <parithosh.jayanthi@ethereum.org>
2023-01-13 18:10:39 +0100

    mod to io

--- terraform/environment/withdrawal-shadowfork-1/ethpandops-io.tf
+++ terraform/environment/withdrawal-shadowfork-1/ethpandops-io.tf
@@ -2,8 +2,8 @@ resource "cloudflare_record" "dns-entry" {
   for_each = {
     for vm in local.digitalocean_vms : "${vm.id}" => vm
   }
-  zone_id = data.sops_file.cloudflare.data["zones.ethpandaops-com.zone_id"]
-  name    = "${each.value.name}.${data.sops_file.cloudflare.data["zones.ethpandaops-com.domain"]}"
+  zone_id = data.sops_file.cloudflare.data["zones.ethpandaops-io.zone_id"]
+  name    = "${each.value.name}.${data.sops_file.cloudflare.data["zones.ethpandaops-io.domain"]}"
   type    = "A"
   value   = "${digitalocean_droplet.main[each.value.id].ipv4_address}"
   proxied = false
