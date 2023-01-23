a73114e223c644f8df5670868a39b6572cea7ce3
parithosh <parithosh.jayanthi@ethereum.org>
2023-01-23 11:28:04 +0530

    adding msf1 configs

---
+++ withdrawal-mainnet-shadowfork-1/custom_config_data/README.md
@@ -0,0 +1,23 @@
+# Mainnet withdrawal shadow fork 1
+
+host withdrawal-shadowfork-1-bootnode-01: 0 - 1000
+host withdrawal-shadowfork-1-lighthouse-besu-01: 1000 - 2000
+host withdrawal-shadowfork-1-lighthouse-erigon-01: 2000 - 2500
+host withdrawal-shadowfork-1-teku-erigon-01: 2500 - 3000
+host withdrawal-shadowfork-1-lighthouse-geth-01: 3000 - 4000
+host withdrawal-shadowfork-1-lighthouse-geth-02: 4000 - 5000
+host withdrawal-shadowfork-1-lighthouse-nethermind-01: 5000 - 6000
+host withdrawal-shadowfork-1-lodestar-besu-01: 6000 - 7000
+host withdrawal-shadowfork-1-lodestar-geth-01: 7000 - 8000
+host withdrawal-shadowfork-1-lodestar-nethermind-01: 8000 - 9000
+host withdrawal-shadowfork-1-nimbus-besu-01: 9000 - 10000
+host withdrawal-shadowfork-1-nimbus-geth-01: 10000 - 11000
+host withdrawal-shadowfork-1-nimbus-nethermind-01: 11000 - 12000
+host withdrawal-shadowfork-1-prysm-besu-01: 12000 - 13000
+host withdrawal-shadowfork-1-prysm-geth-01: 13000 - 14000
+host withdrawal-shadowfork-1-prysm-geth-02: 14000 - 15000
+host withdrawal-shadowfork-1-prysm-nethermind-01: 15000 - 16000
+host withdrawal-shadowfork-1-teku-besu-01: 16000 - 17000
+host withdrawal-shadowfork-1-teku-geth-01: 17000 - 18000
+host withdrawal-shadowfork-1-teku-geth-02: 18000 - 19000
+host withdrawal-shadowfork-1-teku-nethermind-01: 19000 - 20000
--- withdrawal-mainnet-shadowfork-1/generate_keys.sh
+++ withdrawal-mainnet-shadowfork-1/generate_keys.sh
@@ -19,7 +19,7 @@ function prep_group {
     let offset_i=offset+i
     let validators_source_min=offset_i*validators_per_host
     let validators_source_max=validators_source_min+validators_per_host
-    padding=00
+    padding=0
     echo "writing keystores for host $naming_prefix-${padding:${#node_index}:${#padding}}${node_index}: $validators_source_min - $validators_source_max"
     eth2-val-tools keystores \
     --insecure \
@@ -31,5 +31,22 @@ function prep_group {
   done
 }
 
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 0 3 "withdrawal-shadowfork-1-lighthouse-geth" 64
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 0 1 "withdrawal-mainnet-shadowfork-1-bootnode" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 1 1 "withdrawal-mainnet-shadowfork-1-lighthouse-besu" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 4 1 "withdrawal-mainnet-shadowfork-1-lighthouse-erigon" 500
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 5 1 "withdrawal-mainnet-shadowfork-1-teku-erigon" 500
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 3 2 "withdrawal-mainnet-shadowfork-1-lighthouse-geth" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 5 1 "withdrawal-mainnet-shadowfork-1-lighthouse-nethermind" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 6 1 "withdrawal-mainnet-shadowfork-1-lodestar-besu" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 7 1 "withdrawal-mainnet-shadowfork-1-lodestar-geth" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 8 1 "withdrawal-mainnet-shadowfork-1-lodestar-nethermind" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 9 1 "withdrawal-mainnet-shadowfork-1-nimbus-besu" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 10 1 "withdrawal-mainnet-shadowfork-1-nimbus-geth" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 11 1 "withdrawal-mainnet-shadowfork-1-nimbus-nethermind" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 12 1 "withdrawal-mainnet-shadowfork-1-prysm-besu" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 13 2 "withdrawal-mainnet-shadowfork-1-prysm-geth" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 15 1 "withdrawal-mainnet-shadowfork-1-prysm-nethermind" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 16 1 "withdrawal-mainnet-shadowfork-1-teku-besu" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 17 2 "withdrawal-mainnet-shadowfork-1-teku-geth" 1000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 19 1 "withdrawal-mainnet-shadowfork-1-teku-nethermind" 1000
 
