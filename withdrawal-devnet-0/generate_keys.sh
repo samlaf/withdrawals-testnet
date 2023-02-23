d85c07994009e31655656ea5d2556567ae0cc05d
Samuel Laferriere <samlaf92@gmail.com>
2023-02-23 02:05:44 -0800

    some modifs to withdrawal-devnet-0 to test simple setup for our devnet

--- withdrawal-devnet-0/generate_keys.sh
+++ withdrawal-devnet-0/generate_keys.sh
@@ -31,20 +31,20 @@ function prep_group {
   done
 }
 
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 0 1 "withdrawal-devnet-0-bootnode" 2000                       # 0 - 2000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 1 1 "withdrawal-devnet-0-lodestar-geth" 2000                  # 2000 - 4000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 2 1 "withdrawal-devnet-0-teku-geth" 2000                      # 4000 - 6000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 3 1 "withdrawal-devnet-0-lighthouse-geth" 2000                # 6000 - 8000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 4 1 "withdrawal-devnet-0-prysm-geth" 2000                     # 8000 - 10000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 5 1 "withdrawal-devnet-0-lighthouse-nethermind" 2000          # 10000 - 12000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 6 1 "withdrawal-devnet-0-teku-nethermind" 2000                # 12000 - 14000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 7 1 "withdrawal-devnet-0-lodestar-nethermind" 2000            # 14000 - 16000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 8 1 "withdrawal-devnet-0-prysm-nethermind" 2000               # 16000 - 18000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 9 1 "withdrawal-devnet-0-lighthouse-besu" 2000                # 18000 - 20000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 10 1 "withdrawal-devnet-0-teku-besu" 2000                     # 20000 - 22000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 11 1 "withdrawal-devnet-0-lodestar-besu" 2000                 # 22000 - 24000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 12 1 "withdrawal-devnet-0-prysm-besu" 2000                    # 24000 - 26000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 13 1 "withdrawal-devnet-0-lodestar-ethereumjs" 2000           # 26000 - 28000
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 28000 1 "withdrawal-devnet-0-nimbus-besu" 1                   # 28000 - 28001
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 28001 1 "withdrawal-devnet-0-nimbus-geth" 1                   # 28001 - 28002
-prep_group 1 "$VALIDATORS_MNEMONIC_0" 28002 1 "withdrawal-devnet-0-nimbus-nethermind" 1             # 28002 - 28003
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 0 1 "withdrawal-devnet-0-bootnode" 2000                       # 0 - 2000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 1 1 "withdrawal-devnet-0-lodestar-geth" 2000                  # 2000 - 4000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 2 1 "withdrawal-devnet-0-teku-geth" 2000                      # 4000 - 6000
+prep_group 1 "$VALIDATORS_MNEMONIC_0" 0 1 "withdrawal-devnet-0-lighthouse-geth" 2000                # 6000 - 8000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 4 1 "withdrawal-devnet-0-prysm-geth" 2000                     # 8000 - 10000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 5 1 "withdrawal-devnet-0-lighthouse-nethermind" 2000          # 10000 - 12000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 6 1 "withdrawal-devnet-0-teku-nethermind" 2000                # 12000 - 14000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 7 1 "withdrawal-devnet-0-lodestar-nethermind" 2000            # 14000 - 16000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 8 1 "withdrawal-devnet-0-prysm-nethermind" 2000               # 16000 - 18000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 9 1 "withdrawal-devnet-0-lighthouse-besu" 2000                # 18000 - 20000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 10 1 "withdrawal-devnet-0-teku-besu" 2000                     # 20000 - 22000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 11 1 "withdrawal-devnet-0-lodestar-besu" 2000                 # 22000 - 24000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 12 1 "withdrawal-devnet-0-prysm-besu" 2000                    # 24000 - 26000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 13 1 "withdrawal-devnet-0-lodestar-ethereumjs" 2000           # 26000 - 28000
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 28000 1 "withdrawal-devnet-0-nimbus-besu" 1                   # 28000 - 28001
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 28001 1 "withdrawal-devnet-0-nimbus-geth" 1                   # 28001 - 28002
+# prep_group 1 "$VALIDATORS_MNEMONIC_0" 28002 1 "withdrawal-devnet-0-nimbus-nethermind" 1             # 28002 - 28003
